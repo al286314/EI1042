ee0b2e7896eca041c9a09eef91d1df8410e87200
carlostenamarin <al315612@uji.es>
2019-09-24 19:52:47 +0200

    Ejercicio 10

--- P1/partials/registerForm.php
+++ P1/partials/registerForm.php
@@ -6,17 +6,32 @@
 		<label for="nombre">Nombre</label>
 		<br/>
 		<input type="text" name="userName" class="item_requerid" size="20" maxlength="25" value="<?php print $userName ?>"
-		 placeholder="Miguel Cervantes" />
+		 placeholder="Miguel" />
+		<br/>
+		<label for="apellidos">Apellidos</label>
+		<br/>
+		<input type="text" name="userSurname" class="item_requerid" size="20" maxlength="25" value="<?php print $userSurname ?>"
+		 placeholder="Cervantes" />
 		<br/>
 		<label for="email">Email</label>
 		<br/>
 		<input type="text" name="email" class="item_requerid" size="20" maxlength="25" value="<?php print $email ?>"
 		 placeholder="kiko@ic.es" />
 		<br/>
+		<label for="dni">DNI</label>
+		<br/>
+		<input type="text" name="dni" class="item_requerid" size="20" maxlength="9" value="<?php print $dni ?>"
+		 placeholder="12345678A" />
+		<br/>
 		<label for="passwd">Clave</label>
 		<br/>
 		<input type="password" name="passwd" class="item_requerid" size="8" maxlength="25" value="<?php print $passwd ?>"
 		/>
+		<br/>
+		<label for="foto">Foto</label>
+		<br/>
+		<input type="text" name="foto" class="item_requerid" size="20" maxlength="25" value="<?php print $foto ?>"
+		 placeholder="foto.jpg" />
 		<br/>
 		<input type="submit" value="Enviar">
 		<input type="reset" value="Deshacer">
