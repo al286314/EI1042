5b505824d875dd848078c890d3afd082da48570b
carlostenamarin <al315612@uji.es>
2019-11-16 14:03:48 +0100

    Update functions.php

--- P3/twentyseventeen-child-1/functions.php
+++ P3/twentyseventeen-child-1/functions.php
@@ -3,7 +3,7 @@
   // Automatically correct double spaces from any post
   function mfp_Fix_Text_Spacing($the_Post)
   {
-  $the_New_Post = str_replace("chokolandia", " CHOKOLANDIA ", $the_Post);
+  $the_New_Post = str_replace("chokolandia", "CHOKOLANDIA", $the_Post);
   return $the_New_Post;
   }
 
